eae1483eb295cb599aba1f13a0d6ddb2de7b75f4
theahsanusman <ahsanusmanabduljabbar@gmail.com>
2023-03-15 02:20:33 +0500

    added quiz app

---
+++ index.ts
@@ -0,0 +1,45 @@
+import inquirer, { Answers, QuestionCollection } from 'inquirer';
+
+// Define the questions for the quiz
+const questions: QuestionCollection = [
+    {
+        type: 'list',
+        name: 'q1',
+        message: 'What is the capital of France?',
+        choices: ['Paris', 'Berlin', 'Madrid', 'London'],
+    },
+    {
+        type: 'list',
+        name: 'q2',
+        message: 'What is the largest country in the world?',
+        choices: ['Russia', 'China', 'USA', 'India'],
+    },
+    {
+        type: 'list',
+        name: 'q3',
+        message: 'What is the currency of Japan?',
+        choices: ['Yen', 'Dollar', 'Pound', 'Euro'],
+    },
+];
+
+// Define the correct answers for the quiz
+const answers: Answers = {
+    q1: 'Paris',
+    q2: 'Russia',
+    q3: 'Yen',
+};
+
+// Run the quiz
+inquirer.prompt(questions).then((userAnswers: Answers) => {
+    let score = 0;
+
+    // Calculate the user's score
+    Object.entries(userAnswers).forEach(([question, answer]) => {
+        if (answer === answers[question]) {
+            score++;
+        }
+    });
+
+    // Show the user their score
+    console.log(`You scored ${score} out of ${Object.keys(questions).length}`);
+});
